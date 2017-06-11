1f8eb1bb754ebe2b5218968341eb8bf77edc77a8
Kaori Yatsu <s1210103@gmail.com>
2017-06-12 01:46:17 +0900

    [add]ファイルを追加
    .oh-my-zshを追加した

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
  done
  
  # set up oh-my-zsh
-#~/.dotfiles/oh-my-zsh/tools/install.sh | ZSH=~/dotfiles/oh-my-zsh sh
+~/.dotfiles/oh-my-zsh/tools/install.sh | ZSH =~/.dotfiles/oh-my-zsh sh
 
 # create hard-link to oh-my-zsh-powerline-theme from oh-my-zsh/themes
 #ln -f ~/dotfiles/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/dotfiles/oh-my-zsh/themes/powerline.zsh-theme
